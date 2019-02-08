9412f6eece78e5fb198cb37ccb660b3d0f34e2f0
faog <fabiola.orellana.g@gmail.com>
2019-02-08 12:30:19 -0300

    arrow function del boton cifrar en index.js y encode:(valueOffset, valueText) en cipher.js funcionando

--- src/cipher.js
+++ src/cipher.js
@@ -1,7 +1,28 @@
 window.cipher = {
-  encode: () => {
-    /* Acá va tu código */
+  encode: (valueOffset, valueText) => {
+    let result="";
+    let form="";
+    for(let i=0; i<valueText.length; i++){
+      //variable para almacenar el codigo ascii de cada letra
+      let numberAscii=0;
+      numberAscii=valueText.charCodeAt(i);
+      //condicional if para evaluar cada codigo, generando un alert si no es mayuscula o espacio
+      if (numberAscii>=65 && numberAscii<=90) {
+          form = String.fromCharCode(((valueText.charCodeAt(i))-65+parseInt(valueOffset))%26+65);
+          result+=form;  
+      }else if(numberAscii===32){
+          result+=" ";
+      }else if(numberAscii>=97 && numberAscii<=122) {
+          return alert("No esta permitido ingresar letras minúsculas");
+      }else if(numberAscii>=48 && numberAscii<=57){
+          return alert("No esta permitido ingresar dígitos");
+      }else{
+          return alert("No estan permitidos los caracteres especiales");
+      }    
+  }
+  return result;
   },
+  
   decode: () => {
     /* Acá va tu código */
   }
--- src/index.js
+++ src/index.js
@@ -9,10 +9,10 @@ const button_use = document.getElementById('btnUse');
 //Arrow function que ocurre cuando el usuariopresionar el boton "Usar", oculta la sección 1 y muestra la sección 2
 button_use.addEventListener('click',()=>{
 
-let cipherScreen=document.getElementById("boxTwo");
-cipherScreen.style.display="block";
-let welcomeScreen=document.getElementById("boxOne");
-welcomeScreen.style.display="none";  
+    let cipherScreen=document.getElementById("boxTwo");
+    cipherScreen.style.display="block";
+    let welcomeScreen=document.getElementById("boxOne");
+    welcomeScreen.style.display="none";  
 
 })
 
@@ -27,11 +27,25 @@ const button_encode = document.getElementById('btnEncode');
 //Arrow function que ocurre cuando el usuariopresionar el boton "Cifrar", oculta la sección 2 y muestra la sección 3
 button_encode.addEventListener('click',()=>{
 
-let resultEncodeScreen=document.getElementById("boxThree");
-resultEncodeScreen.style.display="block";
+    const inputOffset=document.getElementById('offset')
+    const inputText=document.getElementById('text')
 
-let cipherScreen=document.getElementById("boxTwo");
-cipherScreen.style.display="none";  
+    //variables para rescatar del html los datos ingresados por el usuario
+    let valueOffset=inputOffset.value;
+    let valueText=inputText.value;
+
+    //Mostrar pantalla 3
+    let resultEncodeScreen=document.getElementById("boxThree");
+    resultEncodeScreen.style.display="block";
+
+    let cipherScreen=document.getElementById("boxTwo");
+    cipherScreen.style.display="none";
+
+    document.getElementById("resultText").innerHTML
+    += cipher.encode(valueOffset, valueText);
+  
+    
+
 
 })
 
@@ -41,11 +55,11 @@ const button_decode = document.getElementById('btnDecode');
 //Arrow function que ocurre cuando el usuario presionar el boton "Decifrar", oculta la sección 2 y muestra la sección 3
 button_decode.addEventListener('click',()=>{
 
-let resultDecodeScreen=document.getElementById("boxThree");
-resultDecodeScreen.style.display="block";
+    let resultDecodeScreen=document.getElementById("boxThree");
+    resultDecodeScreen.style.display="block";
 
-let cipherScreen=document.getElementById("boxTwo");
-cipherScreen.style.display="none";  
+    let cipherScreen=document.getElementById("boxTwo");
+    cipherScreen.style.display="none";  
 
 })
 
@@ -59,11 +73,11 @@ oculta la sección 3 y muestra la sección 2*/
 
 button_newText.addEventListener('click',()=>{
 
-let cipherScreen=document.getElementById("boxTwo");
-cipherScreen.style.display="block"; 
+    let cipherScreen=document.getElementById("boxTwo");
+    cipherScreen.style.display="block"; 
 
-let resultDecodeScreen=document.getElementById("boxThree");
-resultDecodeScreen.style.display="none";
+    let resultDecodeScreen=document.getElementById("boxThree");
+    resultDecodeScreen.style.display="none";
 
 })
 
@@ -73,7 +87,6 @@ const button_exit = document.getElementById('btnExit');
 
 /*Arrow function que ocurre cuando el usuario presionar el boton "Decifrar", 
 oculta la sección 3 y muestra la sección 2*/
-
 button_exit.addEventListener('click',()=>{
     
     return alert("Gracias por usar esta aplicación. Hasta la próxima");

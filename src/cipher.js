21a6d9c922df85c4632cb76f5331b84e5d617fba
faog <fabiola.orellana.g@gmail.com>
2019-02-11 19:10:17 -0300

    Condicionales para validar que el usuario ingresa offset correctos y no deja en blanco el campo de texto

--- src/cipher.js
+++ src/cipher.js
@@ -2,23 +2,45 @@ window.cipher = {
   encode: (valueOffset, valueText) => {
     let result="";
     let form="";
+    
+    //Condición que valida que se debe ingresar un offset (no debe ser vacio)
+    if(valueOffset===""){
+      return alert("Debe ingresar un nivel de seguridad")
+    }
+    
+    //Condición que valida que el offset debe ser un number
+    if(isNaN(valueOffset) && valueOffset<1){
+      return alert("El nivel de seguridad debe ser un número mayor a 0")
+    }
+    
+    //Condición que valida que el offset debe ser mayor a 0
+    if(valueOffset<1){
+      return alert("Debe ingresar un nivel de seguridad positivo")
+    }
+
+    //Condición que valida que se debe ingresar un texto (no debe ser vacio)
+    if (valueText==="") {
+      return alert("Debe ingresar un texto")
+    }
+
+    //Recorrer el texto
     for(let i=0; i<valueText.length; i++){
       //variable para almacenar el codigo ascii de cada letra
       let numberAscii=0;
       numberAscii=valueText.charCodeAt(i);
       //condicional if para evaluar cada codigo, generando un alert si no es mayuscula o espacio
       if (numberAscii>=65 && numberAscii<=90) {
-          form = String.fromCharCode(((valueText.charCodeAt(i))-65+parseInt(valueOffset))%26+65);
-          result+=form;  
+        form = String.fromCharCode(((valueText.charCodeAt(i))-65+parseInt(valueOffset))%26+65);
+        result+=form;  
       }else if(numberAscii===32){
-          result+=" ";
+        result+=" ";
       }else if(numberAscii>=97 && numberAscii<=122) {
-          return alert("No esta permitido ingresar letras minúsculas");
+        return alert("No esta permitido ingresar letras minúsculas");
       }else if(numberAscii>=48 && numberAscii<=57){
-          return alert("No esta permitido ingresar dígitos");
+        return alert("No esta permitido ingresar dígitos");
       }else{
-          return alert("No estan permitidos los caracteres especiales");
-      }    
+        return alert("No estan permitidos los caracteres especiales");
+    }    
   }
   
   return `Tu nueva contraseña es: ${result}`;
@@ -27,6 +49,28 @@ window.cipher = {
   decode: (valueOffset, valueText) => {
     let result="";
     let form="";
+
+    //Condición que valida que se debe ingresar un offset (no debe ser vacio)
+    if(valueOffset===""){
+      return alert("Debe ingresar un nivel de seguridad")
+    }
+    
+    //Condición que valida que el offset debe ser un number
+    if(isNaN(valueOffset) && valueOffset<1){
+      return alert("El nivel de seguridad debe ser un número mayor a 0")
+    }
+    
+    //Condición que valida que el offset debe ser mayor a 0
+    if(valueOffset<1){
+      return alert("Debe ingresar un nivel de seguridad positivo")
+    }
+
+    //Condición que valida que se debe ingresar un texto (no debe ser vacio)
+    if (valueText==="") {
+      return alert("Debe ingresar un texto")
+    }
+
+    //Recorrer el texto
     for(let i=0; i<valueText.length; i++){
       //variable para almacenar el codigo ascii de cada letra
       let numberAscii=0;

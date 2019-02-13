48374b09aab6d006aa46fcb0a9ba48fea719cf19
faog <fabiola.orellana.g@gmail.com>
2019-02-12 22:37:02 -0300

    Modificación HE (minúsculas, carácteres y números)

--- src/cipher.js
+++ src/cipher.js
@@ -4,23 +4,28 @@ window.cipher = {
     let form="";
     
     //Condición que valida que se debe ingresar un offset (no debe ser vacio)
-    if(valueOffset===""){
-      return alert("Debe ingresar un nivel de seguridad")
+       
+    if(valueOffset===""){     
+      alert("Debe ingresar un nivel de seguridad");     
+      return false;         
     }
-    
+
+       
     //Condición que valida que el offset debe ser un number
     if(isNaN(valueOffset)){
-      return alert("El nivel de seguridad debe ser un dígito")
+      return alert("El nivel de seguridad debe ser un dígito");
     }
     
     //Condición que valida que el offset debe ser mayor a 0
     if(valueOffset<1){
-      return alert("Debe ingresar un nivel de seguridad positivo")
+      return alert("Debe ingresar un nivel de seguridad positivo");
     }
 
     //Condición que valida que se debe ingresar un texto (no debe ser vacio)
     if (valueText==="") {
-      return alert("Debe ingresar un texto")
+      alert("Debe ingresar un texto");
+      return false;
+      
     }
 
     //Recorrer el texto
@@ -28,22 +33,29 @@ window.cipher = {
       //variable para almacenar el codigo ascii de cada letra
       let numberAscii=0;
       numberAscii=valueText.charCodeAt(i);
-      //condicional if para evaluar cada codigo, generando un alert si no es mayuscula o espacio
+      //condicional if para evaluar cada letra
       if (numberAscii>=65 && numberAscii<=90) {
+        //mayusculas
         form = String.fromCharCode(((valueText.charCodeAt(i))-65+parseInt(valueOffset))%26+65);
         result+=form;  
       }else if(numberAscii===32){
+        //espacios
         result+=" ";
       }else if(numberAscii>=97 && numberAscii<=122) {
-        return alert("No esta permitido ingresar letras minúsculas");
-      }else if(numberAscii>=48 && numberAscii<=57){
-        return alert("No esta permitido ingresar dígitos");
+        //minusculas
+        form = String.fromCharCode(((valueText.charCodeAt(i))-97+parseInt(valueOffset))%26+97);
+        result+=form;  
+      }else if(numberAscii>=33 && numberAscii<=64){
+        //digitos y caracteres
+        form = String.fromCharCode(((valueText.charCodeAt(i))-33+parseInt(valueOffset))%32+33);
+        result+=form; 
       }else{
-        return alert("No estan permitidos los caracteres especiales");
-    }    
+        form=String.fromCharCode(numberAscii);      
+        return alert(`No esta permitido el carácter ${form}`);
+      }    
   }
   
-  return `Tu nueva contraseña es: ${result}`;
+  return `Tu nueva contraseña es: <p>${result}</p>`;
   },
   
   decode: (valueOffset, valueText) => {
@@ -52,43 +64,52 @@ window.cipher = {
 
     //Condición que valida que se debe ingresar un offset (no debe ser vacio)
     if(valueOffset===""){
-      return alert("Debe ingresar un nivel de seguridad")
+      return alert("Debe ingresar un nivel de seguridad");
     }
     
     //Condición que valida que el offset debe ser un number
     if(isNaN(valueOffset)){
-      return alert("El nivel de seguridad debe ser un un dígito")
+      return alert("El nivel de seguridad debe ser un un dígito");
     }
     
     //Condición que valida que el offset debe ser mayor a 0
     if(valueOffset<1){
-      return alert("Debe ingresar un nivel de seguridad positivo")
+      return alert("Debe ingresar un nivel de seguridad positivo");
     }
 
     //Condición que valida que se debe ingresar un texto (no debe ser vacio)
     if (valueText==="") {
-      return alert("Debe ingresar un texto")
+      return alert("Debe ingresar un texto");
     }
 
     //Recorrer el texto
     for(let i=0; i<valueText.length; i++){
       //variable para almacenar el codigo ascii de cada letra
-      let numberAscii=0;
+      let numberAscii=0;      
       numberAscii=valueText.charCodeAt(i);
-      //condicional if para evaluar cada codigo, generando un alert si no es mayuscula o espacio
+      //condicional if para evaluar cada letra
       if (numberAscii>=65 && numberAscii<=90) {
+        //mayusculas
           form = String.fromCharCode(((valueText.charCodeAt(i))-90-parseInt(valueOffset))%26+90);
           result+=form;  
       }else if(numberAscii===32){
+        //espacios
           result+=" ";
       }else if(numberAscii>=97 && numberAscii<=122) {
-          return alert("No esta permitido ingresar letras minúsculas");
-      }else if(numberAscii>=48 && numberAscii<=57){
-          return alert("No esta permitido ingresar dígitos");
+        //minusculas
+        form = String.fromCharCode(((valueText.charCodeAt(i))-122-parseInt(valueOffset))%26+122);
+        result+=form;  
+      }else if(numberAscii>=33 && numberAscii<=64){
+        //digitos y caracteres
+        form = String.fromCharCode(((valueText.charCodeAt(i))-64-parseInt(valueOffset))%32+64);
+        result+=form; 
       }else{
-          return alert("No estan permitidos los caracteres especiales");
-      }    
+        form=String.fromCharCode(numberAscii);      
+        return alert(`No esta permitido el carácter ${form}`);
+      } 
   }
-  return `La contraseña fue generada de: ${result}`;
+  return `La contraseña fue generada de: <p>${result}</p>`;
   }
 };
+
+

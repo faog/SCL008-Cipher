4f600667d8286339f277f080abc2d6027f08d8e5
faog <fabiola.orellana.g@gmail.com>
2019-02-12 22:40:05 -0300

    %Stmts: 76.27, %Branch: 77.27, %Funcs: 100, %Lines:75.44

--- test/cipher.spec.js
+++ test/cipher.spec.js
@@ -18,6 +18,37 @@ describe('cipher', () => {
       assert(cipher.encode(6,"ABC DEF"),"GHI JKL");
     });
 
+    it('debería retornar "hijklmnopqrstuvwxyzabcdefg" para "abcdefghijklmnopqrstuvwxyz" con offset 33',()=>{
+      assert(cipher.encode(33,"abcdefghijklmnopqrstuvwxyz"),"hijklmnopqrstuvwxyzabcdefg");
+    });
+
+    it('debería retornar "ghi jkl" para "abc def" con offset 6',()=>{
+      assert(cipher.encode(6,"abc def"),"ghi jkl");
+    });
+
+    it('debería retornar "ghi jkl" para "abc def" con offset 6',()=>{
+      assert(cipher.encode(6,"abc def"),"ghi jkl");
+    });
+
+    it('debería retornar "Ovsh" para "Hola" con offset 7',()=>{
+      assert(cipher.encode(6,"Hola"),"Ovsh");
+    });
+
+    it('debería retornar "1234567890" para "23456789:1" con offset 1',()=>{
+      assert(cipher.encode(1,"23456789:1"),"1234567890");
+    });
+
+    it("debería retornar $Hola$ para 'Krod' con offset 3",()=>{
+      assert(cipher.encode(3,"'Krod'"),"$Hola$");
+    });
+
+    it("debería retornar ->?Pl= para #>?Fb= con offset 10",()=>{
+      assert(cipher.encode(10,"#>?Fb="),"->?Pl=");
+    });
+   
+    it("debería retornar Idrj 456789:;< para Faog 123456789 con offset 3",()=>{
+      assert(cipher.encode(10,'Faog 123456789'),'Idrj 456789:;<');
+    });
   });
 
   describe('cipher.decode', () => {
@@ -31,9 +62,38 @@ describe('cipher', () => {
     });
 
     it('debería retornar "ABC DEF" para "GHI JKL" con offset 6',()=>{
-      assert(cipher.encode(6,"GHI JKL"),"ABC DEF");
+      assert(cipher.decode(6,"GHI JKL"),"ABC DEF");
+    });
+
+    it('debería retornar "abcdefghijklmnopqrstuvwxyz" para con offset 33',()=>{
+      assert(cipher.encode(33,"hijklmnopqrstuvwxyzabcdefg"),"abcdefghijklmnopqrstuvwxyz");
+    });
+
+    it('debería retornar "abc def" para "ghi jkl" con offset 6',()=>{
+      assert(cipher.decode(6,"ghi jkl"),"abc def");
+    });
+    
+    it('debería retornar "Hola" para "Ovsh" con offset 7',()=>{
+      assert(cipher.decode(6,"Ovsh"),"Hola");
+    });
+
+    it('debería retornar "23456789:1" para "1234567890" con offset 1',()=>{
+      assert(cipher.decode(1,"1234567890"),"23456789:1");
+    });
+
+    it("debería retornar 'Krod' para $Hola$ con offset 3",()=>{
+      assert(cipher.decode(3,"$Hola$"),"'Krod'");
+    });
+
+    it("debería retornar #>?Fb= para ->?Pl= con offset 10",()=>{
+      assert(cipher.decode(10,"->?Pl="),"#>?Fb=");
+    });
+
+    it("debería retornar Faog 123456789 Idrj 456789:;< para Faog 123456789 con offset 3",()=>{
+      assert(cipher.decode(10,'Idrj 456789:;<'),'Faog 123456789');
     });
     
   });
+
 
 });

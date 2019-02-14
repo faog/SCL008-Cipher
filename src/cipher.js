cb9cf574cec3efd16e82ac36791f4a3e19dfd8ac
faog <fabiola.orellana.g@gmail.com>
2019-02-14 10:48:34 -0300

    offset negativo para encode y decode

--- src/cipher.js
+++ src/cipher.js
@@ -11,14 +11,14 @@ window.cipher = {
     }
 
        
-    //Condición que valida que el offset debe ser un number
+    //Condición que valida que el offset debe ser tipo de dato number
     if(isNaN(valueOffset)){
       return alert("El nivel de seguridad debe ser un dígito");
     }
     
-    //Condición que valida que el offset debe ser mayor a 0
-    if(valueOffset<1){
-      return alert("Debe ingresar un nivel de seguridad positivo");
+    //Condición que valida que el offset no debe ser igual a 0
+    if(valueOffset===0){
+      return alert("el nivel de seguridad debe ser distinto a 0");
     }
 
     //Condición que valida que se debe ingresar un texto (no debe ser vacio)
@@ -33,29 +33,57 @@ window.cipher = {
       //variable para almacenar el codigo ascii de cada letra
       let numberAscii=0;
       numberAscii=valueText.charCodeAt(i);
-      //condicional if para evaluar cada letra
-      if (numberAscii>=65 && numberAscii<=90) {
-        //mayusculas
-        form = String.fromCharCode(((valueText.charCodeAt(i))-65+parseInt(valueOffset))%26+65);
-        result+=form;  
-      }else if(numberAscii===32){
-        //espacios
-        result+=" ";
-      }else if(numberAscii>=97 && numberAscii<=122) {
-        //minusculas
-        form = String.fromCharCode(((valueText.charCodeAt(i))-97+parseInt(valueOffset))%26+97);
-        result+=form;  
-      }else if(numberAscii>=33 && numberAscii<=64){
-        //digitos y caracteres
-        form = String.fromCharCode(((valueText.charCodeAt(i))-33+parseInt(valueOffset))%32+33);
-        result+=form; 
+
+      //Condicional para evaluar si el offset es digito negativo o positivo
+
+      //si valueOffset es negativo
+      if(valueOffset<0){
+        if (numberAscii>=65 && numberAscii<=90) {
+          //mayusculas
+            form = String.fromCharCode(((valueText.charCodeAt(i))-90+parseInt(valueOffset))%26+90);
+            result+=form;  
+        }else if(numberAscii===32){
+          //espacios
+            result+=" ";
+        }else if(numberAscii>=97 && numberAscii<=122) {
+          //minusculas
+          form = String.fromCharCode(((valueText.charCodeAt(i))-122+parseInt(valueOffset))%26+122);
+          result+=form;  
+        }else if(numberAscii>=33 && numberAscii<=64){
+          //digitos y caracteres
+          form = String.fromCharCode(((valueText.charCodeAt(i))-64+parseInt(valueOffset))%32+64);
+          result+=form; 
+        }else{
+          form=String.fromCharCode(numberAscii);      
+          return alert(`No esta permitido el carácter ${form}`);
+        } 
+        //si valueOffset es positivo
       }else{
-        form=String.fromCharCode(numberAscii);      
-        return alert(`No esta permitido el carácter ${form}`);
-      }    
-  }
+        //condicional if para evaluar cada letra
+        if (numberAscii>=65 && numberAscii<=90) {
+          //mayusculas
+          form = String.fromCharCode(((valueText.charCodeAt(i))-65+parseInt(valueOffset))%26+65);
+          result+=form;  
+        }else if(numberAscii===32){
+          //espacios
+          result+=" ";
+        }else if(numberAscii>=97 && numberAscii<=122) {
+          //minusculas
+          form = String.fromCharCode(((valueText.charCodeAt(i))-97+parseInt(valueOffset))%26+97);
+          result+=form;  
+        }else if(numberAscii>=33 && numberAscii<=64){
+          //digitos y caracteres
+          form = String.fromCharCode(((valueText.charCodeAt(i))-33+parseInt(valueOffset))%32+33);
+          result+=form; 
+        }else{
+          form=String.fromCharCode(numberAscii);      
+          return alert(`No esta permitido el carácter ${form}`);
+        }    
+      }
+
+    }
   
-  return `Tu nueva contraseña es: <p>${result}</p>`;
+    return `Tu nueva contraseña es: <p>${result}</p>`;
   },
   
   decode: (valueOffset, valueText) => {
@@ -72,9 +100,9 @@ window.cipher = {
       return alert("El nivel de seguridad debe ser un un dígito");
     }
     
-    //Condición que valida que el offset debe ser mayor a 0
-    if(valueOffset<1){
-      return alert("Debe ingresar un nivel de seguridad positivo");
+    //Condición que valida que el offset no debe ser igual a 0
+    if(valueOffset===0){
+      return alert("el nivel de seguridad debe ser distinto a 0");
     }
 
     //Condición que valida que se debe ingresar un texto (no debe ser vacio)
@@ -87,26 +115,54 @@ window.cipher = {
       //variable para almacenar el codigo ascii de cada letra
       let numberAscii=0;      
       numberAscii=valueText.charCodeAt(i);
-      //condicional if para evaluar cada letra
-      if (numberAscii>=65 && numberAscii<=90) {
-        //mayusculas
-          form = String.fromCharCode(((valueText.charCodeAt(i))-90-parseInt(valueOffset))%26+90);
+
+      //Condicional para evaluar si el offset es digito negativo o positivo
+       
+      //si valueOffset es negativo
+      if(valueOffset<0){
+        //condicional if para evaluar cada letra
+        if (numberAscii>=65 && numberAscii<=90) {
+          //mayusculas
+          form = String.fromCharCode(((valueText.charCodeAt(i))-65-parseInt(valueOffset))%26+65);
           result+=form;  
-      }else if(numberAscii===32){
-        //espacios
+        }else if(numberAscii===32){
+          //espacios
           result+=" ";
-      }else if(numberAscii>=97 && numberAscii<=122) {
-        //minusculas
-        form = String.fromCharCode(((valueText.charCodeAt(i))-122-parseInt(valueOffset))%26+122);
-        result+=form;  
-      }else if(numberAscii>=33 && numberAscii<=64){
-        //digitos y caracteres
-        form = String.fromCharCode(((valueText.charCodeAt(i))-64-parseInt(valueOffset))%32+64);
-        result+=form; 
+        }else if(numberAscii>=97 && numberAscii<=122) {
+          //minusculas
+          form = String.fromCharCode(((valueText.charCodeAt(i))-97-parseInt(valueOffset))%26+97);
+          result+=form;  
+        }else if(numberAscii>=33 && numberAscii<=64){
+          //digitos y caracteres
+          form = String.fromCharCode(((valueText.charCodeAt(i))-33-parseInt(valueOffset))%32+33);
+          result+=form; 
+        }else{
+          form=String.fromCharCode(numberAscii);      
+          return alert(`No esta permitido el carácter ${form}`);
+        }    
+      //si valueOffset es negativo
       }else{
-        form=String.fromCharCode(numberAscii);      
-        return alert(`No esta permitido el carácter ${form}`);
-      } 
+          //condicional if para evaluar cada letra
+        if (numberAscii>=65 && numberAscii<=90) {
+          //mayusculas
+            form = String.fromCharCode(((valueText.charCodeAt(i))-90-parseInt(valueOffset))%26+90);
+            result+=form;  
+        }else if(numberAscii===32){
+          //espacios
+            result+=" ";
+        }else if(numberAscii>=97 && numberAscii<=122) {
+          //minusculas
+          form = String.fromCharCode(((valueText.charCodeAt(i))-122-parseInt(valueOffset))%26+122);
+          result+=form;  
+        }else if(numberAscii>=33 && numberAscii<=64){
+          //digitos y caracteres
+          form = String.fromCharCode(((valueText.charCodeAt(i))-64-parseInt(valueOffset))%32+64);
+          result+=form; 
+        }else{
+          form=String.fromCharCode(numberAscii);      
+          return alert(`No esta permitido el carácter ${form}`);
+        } 
+      }
   }
   return `La contraseña fue generada de: <p>${result}</p>`;
   }
